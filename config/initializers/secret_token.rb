b477e3a6ea9dad3ae911df6c48bd47d0b3a850bb
Josue Figueroa <jfigueroagama@redesvozdatos.com>
2013-11-11 20:30:16 -0800

    Initial commit Ticketee 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticketee4::Application.config.secret_key_base = 'e593a90bd709d3f59eb7feeefca6d1239be2fd5a9b9c432c70f63d16afd175849901d1777a84f1dbb518ee6bf4ae2b37bc8dee448159b83bc373d81ec4d41783'
