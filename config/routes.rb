e8d49341b3ca63d7a2392d5b15d8f892d65401e1
Josue Figueroa <jfigueroagama@redesvozdatos.com>
2013-12-08 13:22:52 -0800

    Implement creating Tickets for a Project

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Ticketee4::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'projects#index'
   
-  resources :projects
+  # nested resource
+  resources :projects do
+    resources :tickets
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
